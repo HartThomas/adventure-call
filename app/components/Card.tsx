7ff632e9a96174c07c85592beb3c0d4b0cec6743
HartThomas <t.j.hart89@hotmail.co.uk>
2023-08-07 10:42:23 +0100

    Add initial styling

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -12,9 +12,19 @@ export default function Card(props: Tile) {
     <div
       key={identifier}
       id={identifier}
-      className="border-gray-200 border border-double w-20 border-4 h-20 justify-center items-center flex"
+      className="border-gray-200 border w-20 border-4 h-20 justify-center items-center flex rounded-lg shadow-md bg-green-100"
     >
-      <p className="text-4xl font-mono">{props.stage[0].toUpperCase()}</p>
+      <p
+        className={`text-4xl font-mono text-orange-900 ${
+          props.stage === "grass"
+            ? "animate-none"
+            : props.stage === "teleport"
+            ? "animate-spin"
+            : "animate-none"
+        }`}
+      >
+        {props.stage === "grass" ? " " : props.stage[0].toUpperCase()}
+      </p>
     </div>
   );
 }
